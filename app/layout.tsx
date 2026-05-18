d06ce85d0bf1132d6c3decf962b1ad67c01088b2
Focus Club <focusclub.adm@gmail.com>
2026-05-18 04:17:17 -0300

    corrige zoom ios safari inputs

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
+  userScalable: false,
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
